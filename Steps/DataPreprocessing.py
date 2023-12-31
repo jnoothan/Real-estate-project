6be13f6a27db3b9946ba987fffcc06076c5057a4
jnoothan998 <jnoothan998@gmail.com>
2024-01-01 03:37:59 +0530

    EDA

--- Steps/DataPreprocessing.py
+++ Steps/DataPreprocessing.py
@@ -3,6 +3,25 @@ import logging
 import pandas as pd
 
 #preprocess data - handle missing values
+class DataPreprocess:
+    def __init__(self,df)-> pd.DataFrame:
+        self.df=df
+        
+    def data_transformation(self,columns=['bed','bath','house_size','price']):
+        self.columns=columns
+        for i in self.df[self.columns]:
+            if (self.df[i].dtype) == 'float64':
+                self.df[i]=self.df[i].astype('int32')
+                
+    def feature_selection(self,drop_columns=['status']):
+        self.drop_columns=drop_columns
+        self.df=self.df.drop(columns=drop_columns)
+        
+    def x_y_split(self,)
+        
+        
+        
+        
 
 #drop unnecessary features
 
