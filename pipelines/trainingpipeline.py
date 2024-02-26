dd2c28e098ef32ee2c7462104c065702de741eda
jnoothan998 <jnoothan998@gmail.com>
2024-02-27 00:54:34 +0530

    model_evaluations

--- pipelines/trainingpipeline.py
+++ pipelines/trainingpipeline.py
@@ -1,15 +1,18 @@
 from zenml import pipeline
-from Steps.DataValidation import DataValidation # returns csv files in ingested data folder
-from Steps.DataIngestion import data_ingestion # returns df ingests data as df into ingested data folder or invalid data folder
-from Steps.DataPreprocessing import split_data # returns x_train,X_test,y_train,y_test
-from Steps.TrainModel import trained_model
+from ..Steps.DataValidation import DataValidation  # Module to validate data
+from ..Steps.DataIngestion import data_ingestion  # Module to ingest data
+from ..Steps.DataPreprocessing import split_data  # Module to split data
+from ..Steps.TrainModel import trained_model  # Module to train the model
+from ..Steps.evaluation import model_accuracy  # Module for model evaluation
 
 @pipeline()
 def trainingpipeline():
-    folder_path='/Data for ml'
-    DataValidation(folder_path)
-    df=data_ingestion(Ingested Data)
-    x_train,x_test,y_train,y_test=split_data(df)
-    trained_model(x_train,x_test,y_train,y_test)
+    folder_path = '/Data for ml'
+    DataValidation(folder_path)  # Validate data
+    df = data_ingestion(folder_path)  # Ingest data
+    x_train, x_test, y_train, y_test = split_data(df)  # Split data
+    trained_model(x_train, x_test, y_train, y_test)  # Train the model
+    score, mse, r2, rmse = model_accuracy(x_train, x_test, y_train, y_test)  # Evaluate model accuracy
 
-
+if __name__ == '__main__':
+    trainingpipeline()
