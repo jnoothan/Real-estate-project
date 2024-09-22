e32969fe4fd3e804ba747a0e11c1f1060f22cb85
noothana prasanna <jnoothan998@gmail.com>
2024-09-22 15:18:55 +0530

    flask_donefgdfgdg

--- pipelines/trainingpipeline.py
+++ pipelines/trainingpipeline.py
@@ -1,16 +1,14 @@
 import sys
 # Add the parent directory of 'Steps' package to the Python path
 sys.path.append('F:/Data Science/ML Projects 22-09-2023/Real estate project')
-import pandas as pd
-import os
 from zenml import pipeline
 from Steps.DataValidation import DataValidation  # Module to validate data
-from Steps.DataIngestion2 import data_ingestion  # Module to ingest data
+from Steps.DataIngestion import data_ingestion  # Module to ingest data
 from Steps.DataPreprocessing import split_data  # Module to split data
 from Steps.TrainModel import trained_model  # Module to train the model
 from Steps.evaluation import model_accuracy  # Module for model evaluation # Module for model Prediction flask
 
-@pipeline(enable_cache=True)
+@pipeline(enable_cache=False)
 def trainingpipeline():
     folder_path = './Data for ml'
     DataValidation(folder_path)
